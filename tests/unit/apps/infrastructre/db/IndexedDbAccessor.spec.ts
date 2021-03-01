a401c812f922433401852f0e71fd60c0acc24ce2
Satoshi Yamashita <sympathia3104@gmail.com>
2021-03-01 23:24:05 +0900

    chore: practice jest test

---
+++ tests/unit/apps/infrastructre/db/IndexedDbAccessor.spec.ts
@@ -0,0 +1,8 @@
+import { IndexedDBAccessor } from '@/apps/infrastracture/db/indexedDb/IndexedDbAccessor.ts';
+
+test('insert', () => {
+  const accessor = new IndexedDBAccessor();
+  expect(() => {
+    accessor.insert(0)
+  }).toThrow('Method not implemented.');
+});
